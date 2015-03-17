7e7b7d37c41266e26434eb29a31e5569258f0624
Dave Knapik <daveknapik@gmail.com>
2015-03-17 00:36:14 -0400

    Moved api calling code into the Resource model

--- app/controllers/explorer_controller.rb
+++ app/controllers/explorer_controller.rb
@@ -6,21 +6,6 @@ class ExplorerController < ApplicationController
 
   def get_api_response
     resource = Resource.find params[:resource_id]
-
-    path = resource.path
-
-    resource.parameters.select {|p| p.url_parameter}.each do |parameter|
-      path.gsub!(":#{parameter.name}", params[:parameters][parameter.name])
-      params[:parameters].delete parameter.name
-    end
-
-    api_response = HTTParty.send(
-                    resource.http_method.downcase,
-                    "http://api.testing.iknow.jp" + path, 
-                    query: params[:parameters],
-                    basic_auth: {username: params[:username], password: params[:password]},
-                    debug_output: $stdout)
-    parsed_api_response_body = JSON.parse(api_response.body)
-    render json: parsed_api_response_body
+    render json: resource.get_api_response(params)
   end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,9 +1,30 @@
 class Resource < ActiveRecord::Base
+  include HTTParty
+  base_uri "http://api.testing.iknow.jp"
+
   belongs_to :api_call
   has_many :parameters
   has_many :examples
 
   def path_with_http_method
     "#{http_method} #{path}"
+  end
+
+  def get_api_response(params)
+    path_with_parameter_values = self.path
+
+    self.parameters.select {|p| p.url_parameter}.each do |parameter|
+      path_with_parameter_values.gsub!(":#{parameter.name}", params[:parameters][parameter.name])
+      params[:parameters].delete parameter.name
+    end
+
+    api_response = self.class.send(
+                    self.http_method.downcase,
+                    path_with_parameter_values, 
+                    query: params[:parameters].reject {|k,v| v.empty?},
+                    basic_auth: {username: params[:username], password: params[:password]},
+                    debug_output: $stdout)
+    
+    JSON.parse(api_response.body)
   end
 end
